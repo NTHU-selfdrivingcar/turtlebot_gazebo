4988fc195c542cd0f66c0b58f6a6c9c750748eeb
Dennis <liusx880630@gmail.com>
2021-03-29 19:07:48 +0800

    add subscribe to goal

--- turtlebot_teleop/src/trace_target.cpp
+++ turtlebot_teleop/src/trace_target.cpp
@@ -58,6 +58,7 @@ int main(int argc, char ** argv){
     ros::NodeHandle n;
     ros::Publisher publish = n.advertise<geometry_msgs::Twist>("cmd_vel",1000);
     ros::Subscriber pose = n.subscribe("robot_pose", 1000, loop_pose);
+    ros::Subscriber target = n.subscribe("move_base_simple/goal", 1000, loop_goal);
     int rate = 10;
     ros::Rate loop_rate(rate);
 
